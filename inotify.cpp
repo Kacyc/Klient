e588cbf8907850a1e40a9e6cc5ea54e398c0f6f2
Kacyc <kacyc@wp.pl>
2015-12-29 15:59:55 +0100

    dodanie zawartosci klasy inotify

--- inotify.cpp
+++ inotify.cpp
@@ -1,1 +1,55 @@
 #include "inotify.h"
+
+Inotify::Inotify(std::string path)
+{
+  this->path=path;
+  
+  this->fd = inotify_init();
+
+  if ( fd < 0 ) {
+    perror( "inotify_init" );
+  }
+};
+
+/*std:string*/void Inotify::read()
+{
+  wd = inotify_add_watch( fd, path, IN_MODIFY | IN_CREATE | IN_DELETE );
+   length = read( fd, buffer, BUF_LEN );  
+
+  if ( length < 0 ) {
+    perror( "read" );
+  }  
+
+  while ( i < length ) {
+    struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+    if ( event->len ) {
+      if ( event->mask & IN_CREATE ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was created.\n", event->name );       
+        }
+        else {
+          printf( "The file %s was created.\n", event->name );
+        }
+      }
+      else if ( event->mask & IN_DELETE ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was deleted.\n", event->name );       
+        }
+        else {
+          printf( "The file %s was deleted.\n", event->name );
+        }
+      }
+      else if ( event->mask & IN_MODIFY ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was modified.\n", event->name );
+        }
+        else {
+          printf( "The file %s was modified.\n", event->name );
+        }
+      }
+    }
+    i += EVENT_SIZE + event->len;
+  }
+  ( void ) inotify_rm_watch( fd, wd );
+  //return
+}
--- inotify.h
+++ inotify.h
@@ -1,2 +1,25 @@
+#ifndef INOTIFY_H
+#define INOTIFY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <sys/inotify.h>
+#include <string>
 
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+class Inotify
+{
+private:
+  std::string path;
+  int fd;
+  int wd;
+public:
+  Inotify(std::string path);
+  /*std::string*/void read();
+};
+
+
+#endif
